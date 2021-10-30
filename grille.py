1bbe71416edf98091e232e1e1296227059af7819
Romain Seznec <contact@romainseznec.com>
2021-10-30 15:57:20 +0200

    Spécifications

--- grille.py
+++ grille.py
@@ -1,19 +1,40 @@
 from random import choice
 
 def generation_couleurs(taille):
-    """Génère une grille de couleurs aléatoire selon une taille donnée.
-
-    Paramètre :
-        - taille (int): Définie la taille de la liste et des sous-listes renvoyées.
-    
-    Retourne :
-        list: Liste de sous-listes contenant les valeurs aléatoires sélectionnées parmis la liste de couleurs par défaut.
     """
+    Génération aléatoire de la grille de couleurs.
+
+    Paramètres
+    ----------
+    taille : int
+        Taille de la grille.
+
+    Renvois
+    -------
+    list
+        Grille de couleurs.
+    """ 
     defauts = ["red", "blue", "yellow", "green", "purple", "white"]
     return [[choice(defauts) for _ in range(taille)] for _ in range(taille)]
 
 def colorier(grille, i , j, couleur_initiale, couleur_finale):
-    """À l'aide d'une grille de couleur donnée, effectue la propagation d'une couleur initiale vers une nouvelle couleur au point (i, j).
+    """
+    Propagation des couleurs sur la grille.
+
+    Propage la couleur finale au point i, j et à toutes les cases adjacentes (côte à côte horizontalement ou verticalement) de même couleur initiale.
+
+    Paramètres
+    ----------
+    grille : list
+        Grille de couleurs à modifier.
+    i : int
+        Position en abscisse du point à partir duquel propager la couleur.
+    j : int
+        Position en ordonnée du point à partir duquel propager la couleur.
+    couleur_initiale : str
+        Couleur initiale du point i, j.
+    couleur_finale : str
+        Couleur à propager.
     """
     if not(0 <= i < len(grille) and 0 <= j < len(grille)) or grille[j][i] != couleur_initiale or couleur_initiale == couleur_finale:
         return
@@ -25,7 +46,19 @@ def colorier(grille, i , j, couleur_initiale, couleur_finale):
     colorier(grille, i - 1, j, couleur_initiale, couleur_finale)
     colorier(grille, i, j - 1, couleur_initiale, couleur_finale)
 
-def generation_grille(couleurs, canvas):
+def afficher_rectangles(couleurs, canvas):
+    """
+    Construit la partie graphique de la grille.
+
+    À partir de la grille de couleurs, création des rectangles affichés par l'intermédiaire du canvas donné.
+
+    Paramètres
+    ----------
+    couleurs : list
+        Grille de couleurs.
+    canvas : tkinter.Canvas
+        Canvas Tkinter à utiliser pour la création des rectangles.
+    """
     x = 0
     y = 0
 
@@ -36,7 +69,21 @@ def generation_grille(couleurs, canvas):
         y += 50
         x = 0
 
-def attention_aux_couleurs(couleurs, canvas, info_clic):
+def attention_pour_les_couleurs(couleurs, canvas, info_clic):
+    """
+    Action executée au clic sur la grille graphique.
+
+    Propage la couleur cliquée à partir du point 0, 0 selon les conditions énumérées dans la fonction colorier. Met à jour la partie graphique de la grille.
+
+    Paramètres
+    ----------
+    couleurs : list
+        Grille de couleurs à modifier.
+    canvas : tkinter.Canvas
+        Canvas Tkinter à modifier.
+    info_clic : tkinter.Event
+        Évènement Tkinter contenant les informations sur le clic.
+    """ 
     colorier(couleurs, 0, 0, couleurs[0][0], couleurs[info_clic.y // 50][info_clic.x // 50])
-    canvas.delete("all")
-    generation_grille(couleurs, canvas)
+    canvas.delete("all")  # Supprime tous les rectangles du Canvas pour libérer la mémoire
+    afficher_rectangles(couleurs, canvas)
